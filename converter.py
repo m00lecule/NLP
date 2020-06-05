e0a19d86e95807bd8cae0bffd4c26928d69ad6ff
mqqlecule <michaldygaz@protonmail.com>
2020-06-05 02:30:40 +0200

    textcorp strip

--- converter.py
+++ converter.py
@@ -9,28 +9,35 @@ demoji.download_codes()
 dfr = pd.read_excel("robert.xlsx")
 dfj = pd.read_excel("janusz.xlsx")
 
-
-dfr["Text"] = dfr["Text"].apply(
-    lambda x: re.sub(r"http\S+|(RT)? ?@[^\s]+ :?|\n|#[^\s]+", "", x)
-)
-
-dfj["Text"] = dfj["Text"].apply(
-    lambda x: re.sub(r"http\S+|(RT)? ?@[^\s]+ :?|\n|#[^\s]+", "", x)
-)
-
-
-dfj["Text"] = dfj["Text"].apply(lambda x: re.sub(r"$ +", "", x))
-
 result = pd.concat([dfr[["Text", "Name"]], dfj[["Text", "Name"]]])
 
-result["Text"].replace("", np.nan, inplace=True)
 
 result["Text"] = result["Text"].apply(lambda x: demoji.replace(str(x), ""))
 
+
+result["Text"] = result["Text"].apply(
+    lambda x: re.sub(r"http\S+|(RT)? ?@[^\s]+ :?|\n|#[^\s]+|\|", "", x)
+)
+
+# dfj["Text"] = dfj["Text"].apply(
+#     lambda x: re.sub(r"http\S+|(RT)? ?@[^\s]+ :?|\n|#[^\s]+|\|", "", x)
+# )
+
+result["Text"] = result["Text"].apply(lambda x: x.rstrip())
+
+result["Text"] = result["Text"].apply(lambda x: x.lstrip())
+
+
+result["Text"] = result["Text"].apply(lambda x: re.sub(r"$ +", "", x))
+
+
+result["Text"].replace("", np.nan, inplace=True)
+
 result.dropna(subset=["Text"], inplace=True)
+
 
 # result = result[~result["Text"].isin([""])]
 
-result.to_csv("quotes1.csv", sep="|", index=False)
+result.to_csv("quotes1.csv", sep="#", index=False)
 
-print(result["Text"])
+# print(result["Text"])
